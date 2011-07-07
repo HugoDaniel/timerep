c36acb67280cdc8772e672c767f0f9dac7527c22
Hugo Daniel <mr.hugo.gomes@gmail.com>
2011-07-07 18:43:18 +0100

    Changed the name of the package and added support for RFC2822

--- Data/Time/RFC2822.hs
+++ Data/Time/RFC2822.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 -- |                                                                               
--- Module      : Data.Time.RFC3339
+-- Module      : Data.Time.RFC2822
 -- Copyright   : (c) 2011 Hugo Daniel Gomes
 --
 -- License     : BSD-style
@@ -9,14 +9,14 @@
 -- Portability : GHC
 --
 -- Support for reading and displaying time in the format specified by 
--- the RFC3339 <http://www.ietf.org/rfc/rfc3339.txt>
+-- the RFC2822 <http://www.ietf.org/rfc/rfc2822.txt> section 3.3
 --
 -- Example of usage:
 -- >
 -- > import Data.Time.LocalTime
 -- >
 -- > showTime :: IO String
--- > showTime = getZonedTime >>= return . showRFC3339
+-- > showTime = getZonedTime >>= return . showRFC2822
 -- >
 -- > example1 = "1985-04-12T23:20:50.52Z"
 -- > example2 = "1996-12-19T16:39:57-08:00"
@@ -27,10 +27,10 @@
 -- >
 -- > readAll = map readRFC3339 examples
 
-module Data.Time.RFC3339 (
+module Data.Time.RFC2822 (
     -- * Basic type class
     -- $basic
-    RFC3339(showRFC3339, readRFC3339)
+    -- TODO: RFC2822(showRFC2822, readRFC2822)
 ) where
 
 import Data.Time.Format
@@ -39,36 +39,42 @@ import Data.Time.Calendar
 import Data.Maybe 
 import System.Locale
 
-test1 = "1985-04-12T23:20:50.52Z"
-test2 = "1996-12-19T16:39:57-08:00"
-test3 = "1990-12-31T23:59:60Z"
-test4 = "1990-12-31T15:59:60-08:00"
-test5 = "1937-01-01T12:00:27.87+00:20"
-tests = [test1, test2, test3, test4, test5]
-testParse = length (catMaybes (map readRFC3339 tests)) == length tests
+test1 = "Fri, 21 Nov 1997 09:55:06 -0600"
+test2 = "Tue, 15 Nov 1994 12:45:26 GMT"
+test3 = "Tue, 1 Jul 2003 10:52:37 +0200"
+test4 = "Thu, 13 Feb 1969 23:32:54 -0330"
+test5 = "Mon, 24 Nov 1997 14:22:01 -0800"
+test6 = "Thu,          13\n     Feb\n  1969\n        23:32\n     -0330"
+test7 = "Thu,          13\n     Feb\n  1969\n        23:32\n     -0330 (Newfoundland Time)"
+test8 = "24 Nov 1997 14:22:01 -0800"
+test9 = "15 Nov 1994 12:45:26 GMT"
+tests = [test1, test2, test3, test4, test5, test6, test7, test8, test9]
+testParse = length (catMaybes (map readRFC2822 tests)) == length tests
 
 -- ----------------------------------------------------------------------------
--- The RFC3339 class definition
+-- The RFC2822 class definition
 
 -- | This class is here to allow future support for other data types 
 -- like Data.Text or Data.ByteString if that becomes necessary
-class RFC3339 a where
-  showRFC3339 :: ZonedTime -> a
-  readRFC3339 :: a -> Maybe ZonedTime
+class RFC2822 a where
+  showRFC2822 :: ZonedTime -> a
+  readRFC2822 :: a -> Maybe ZonedTime
 
 -- | For now there is only an instance for the String data type
-instance RFC3339 String where
-  showRFC3339 zt@(ZonedTime lt z) = 
-    formatTime defaultTimeLocale "%FT%T" zt ++ printZone
+instance RFC2822 String where
+  showRFC2822 zt@(ZonedTime lt z) = 
+    formatTime defaultTimeLocale "%a, %e %b %Y %T" zt ++ printZone
     where
       timeZoneStr = timeZoneOffsetString z
       printZone = if timeZoneStr == timeZoneOffsetString utc
-                    then "Z"
-                    else take 3 timeZoneStr ++ ":" ++ drop 3 timeZoneStr
-  readRFC3339 t = foldr (tryP t) Nothing [ p "%FT%TZ"
-                                         , p "%FT%T%z"
-                                         , p "%FT%T%Q%z"
-                                         , p "%FT%T%QZ"
+                    then " GMT"
+                    else " " ++ timeZoneStr
+
+  readRFC2822 t = foldr (tryP t) Nothing [ p "%a, %e %b %Y %T GMT"
+                                         , p "%a, %e %b %Y %T %z"
+                                         , p "%e %b %Y %T GMT"
+                                         , p "%e %b %Y %T %z"
+                                         -- , p "%FT%T%QZ"
                                          ]
     where 
       p :: String -> String -> Maybe ZonedTime
@@ -79,4 +85,4 @@ instance RFC3339 String where
                    | otherwise = f s
 
 showTime :: IO String
-showTime = getZonedTime >>= return . showRFC3339
+showTime = getZonedTime >>= return . showRFC2822
